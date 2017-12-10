13c07a4aa17f054f45206ab34d7b063c9a9065db
Maksym Stupakov <maksym.stupakov@gmail.com>
2017-12-10 03:34:48 +0200

    Updated code to call destructors for global objects.

--- cxxabi.c
+++ cxxabi.c
@@ -6,8 +6,17 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/slab.h>
 
 void *__dso_handle = 0;
+
+static struct atexit_t {
+  void *arg;
+  void *dso;
+  void (*cb)(void*);
+
+  struct atexit_t *next;
+} *head;
 
 void __cxa_pure_virtual(void) {
   printk(KERN_INFO "! Enter: %s\n", __FUNCTION__);
@@ -26,7 +35,40 @@ void __cxa_guard_release(void *p) {
   printk(KERN_INFO "! Enter: %s\n", __FUNCTION__);
 }
 
-int __cxa_atexit(void (*f) (void*), void *arg, void *dso_handle) {
-  printk(KERN_INFO "! Enter: %s\n", __FUNCTION__);
+int __cxa_atexit(void (*cb)(void*), void *arg, void *dso_handle) {
+  struct atexit_t *p = kmalloc(sizeof(struct atexit_t), GFP_KERNEL);
+  if (!p) {
+    return -1;
+  }
+
+  p->cb = cb;
+  p->arg = arg;
+  p->dso = dso_handle;
+
+  p->next = head;
+  head = p;
+
   return 0;
 }
+
+void __cxa_finalize(void *dso_handle) {
+  struct atexit_t *p = head;
+
+  while (p) {
+    struct atexit_t *f = p;
+
+    p->cb(p->arg);
+    p = p->next;
+
+    kfree(f);
+  }
+
+  head = 0;
+}
+
+void __cxa_finalize_dso(void) {
+  printk(KERN_INFO "! Enter: %s\n", __FUNCTION__);
+  __cxa_finalize(__dso_handle);
+}
+
+void (*p)(void) __attribute__ ((section(".fini_array"))) = __cxa_finalize_dso;
